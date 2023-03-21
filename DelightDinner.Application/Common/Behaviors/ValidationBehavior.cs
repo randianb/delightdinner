08d9610395b27e24711039859d5016dfbd206c63
Andrii Korshenko <108221203+andriikorshenko@users.noreply.github.com>
2023-03-21 19:32:41 +0100

    Add ValidationBehavior and RegisterCommandValidator

---
+++ DelightDinner.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,38 @@
+﻿using DelightDinner.Application.Authentication.Commands.Register;
+using DelightDinner.Application.Authentication.Common;
+using ErrorOr;
+using FluentValidation;
+using MediatR;
+
+namespace DelightDinner.Application.Common.Behaviors;
+
+public class ValidationRegisterCommandBehavior
+    : IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>
+{
+    private readonly IValidator<RegisterCommand> _validator;
+
+    public ValidationRegisterCommandBehavior(IValidator<RegisterCommand> validator)
+    {
+        _validator = validator;
+    }
+
+    public async Task<ErrorOr<AuthenticationResult>> Handle(
+        RegisterCommand request,
+        RequestHandlerDelegate<ErrorOr<AuthenticationResult>> next,
+        CancellationToken cancellationToken)
+    {
+        var validatioResult = await _validator.ValidateAsync(request, cancellationToken);
+
+        if (validatioResult.IsValid)
+        {
+            return await next();
+        }
+
+        var errors = validatioResult.Errors
+            .ConvertAll(validationFailure => Error.Validation(
+                validationFailure.PropertyName,
+                validationFailure.ErrorMessage));
+        
+        return errors;
+    }
+}
--- DelightDinner.Application/DependencyInjection.cs
+++ DelightDinner.Application/DependencyInjection.cs
@@ -1,5 +1,11 @@
-﻿using MediatR;
+﻿using DelightDinner.Application.Authentication.Commands.Register;
+using DelightDinner.Application.Authentication.Common;
+using DelightDinner.Application.Common.Behaviors;
+using ErrorOr;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace DelightDinner.Application;
 
@@ -8,6 +14,12 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(typeof(DependencyInjection).Assembly);
+
+        services.AddScoped<
+            IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>, 
+            ValidationRegisterCommandBehavior>();
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         return services;
     }
