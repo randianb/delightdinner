33166487c7455f45404f65c1220edb4ac3ec5c36
Andrii Korshenko <108221203+andriikorshenko@users.noreply.github.com>
2023-03-26 20:57:42 +0200

    Implement Entity and AggregateRoot abstractions

---
+++ DelightDinner.Domain/Common/Models/AggregateRoot.cs
@@ -0,0 +1,10 @@
+﻿namespace DelightDinner.Domain.Common.Models;
+
+public abstract class AggregateRoot<TId> : Entity<TId>
+    where TId : notnull
+{
+    protected AggregateRoot(TId id) : base(id) 
+    {
+        
+    }
+}
