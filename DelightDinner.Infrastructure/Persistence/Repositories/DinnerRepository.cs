b0f1b838584d82d44ba92be45b2c33c3bf8a67a5
andriikorshenko <andriikorshenko@gmail.com>
2023-04-22 13:04:01 +0200

    Add DinnerRepository

---
+++ DelightDinner.Infrastructure/Persistence/Repositories/DinnerRepository.cs
@@ -0,0 +1,27 @@
+﻿using DelightDinner.Domain.Dinner;
+using DelightDinner.Domain.Host.ValueObjects;
+
+namespace DelightDinner.Infrastructure.Persistence.Repositories;
+
+public class DinnerRepository
+{
+    private readonly DelightDinnerDbContext _dbContext;
+
+    public DinnerRepository(DelightDinnerDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public void Add(Dinner dinner)
+    {
+        _dbContext.Add(dinner);
+        _dbContext.SaveChanges();
+    }
+
+    public List<Dinner> List(HostId hostId)
+    {
+        return _dbContext.Dinners
+            .Where(x => x.HostId == hostId)
+            .ToList();
+    }
+}
