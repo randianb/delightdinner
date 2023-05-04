9ac23983421649e7950a745efa877347c78ce504
andriikorshenko <andriikorshenko@gmail.com>
2023-05-04 21:44:32 +0200

    Add ListMenus Queries to the Application layer

---
+++ DelightDinner.Application/Menus/Queries/ListMenus/ListMenuQuery.cs
@@ -0,0 +1,9 @@
+﻿using DelightDinner.Domain.Menu;
+
+using ErrorOr;
+using MediatR;
+
+namespace DelightDinner.Application.Menus.Queries.ListMenus;
+
+public record ListMenuQuery(string HostId)
+    : IRequest<ErrorOr<List<Menu>>>;
---
+++ DelightDinner.Application/Menus/Queries/ListMenus/ListMenuQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using DelightDinner.Application.Common.Interfaces.Persistence;
+using DelightDinner.Domain.Host.ValueObjects;
+using DelightDinner.Domain.Menu;
+
+using ErrorOr;
+using MediatR;
+
+namespace DelightDinner.Application.Menus.Queries.ListMenus;
+
+public class ListMenuQueryHandler : IRequestHandler<ListMenuQuery, ErrorOr<List<Menu>>>
+{
+    private readonly IMenuRepository _menuRepository;
+
+    public ListMenuQueryHandler(IMenuRepository menuRepository)
+    {
+        _menuRepository = menuRepository;
+    }
+
+    public async Task<ErrorOr<List<Menu>>> Handle(ListMenuQuery query, CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
+        
+        var hostId = HostId.Create(query.HostId);
+
+        return _menuRepository.List(hostId);
+    }
+}
