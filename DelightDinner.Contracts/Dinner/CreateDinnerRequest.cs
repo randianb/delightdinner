09ab994018beceea3e6514641ac0eabd2a5ca6ea
andriikorshenko <andriikorshenko@gmail.com>
2023-05-04 21:33:26 +0200

    Add Dinner Contracts for Create (Request/Response)

--- DelightDinner.Contracts/Dinner/CreateDinnerRequest.cs
+++ DelightDinner.Contracts/Dinner/CreateDinnerRequest.cs
@@ -1,6 +1,23 @@
 ﻿namespace DelightDinner.Contracts.Dinner;
 
-public class CreateDinnerRequest
-{
+public record CreateDinnerRequest(
+    string Name,
+    string Description,
+    DateTime StartDateTime,
+    DateTime EndDateTime,
+    int MaxGuests,
+    bool IsPublic,
+    DinnerPrice Price,
+    string MenuId,
+    Uri ImageUri,
+    DinnerLocation Location);
 
-}
+public record DinnerPrice(
+    decimal Amount,
+    string Currency);
+
+public record DinnerLocation(
+    string Name,
+    string Address,
+    double Latitude,
+    double Longtitude);
--- DelightDinner.Contracts/Dinner/DinnerResponse.cs
+++ DelightDinner.Contracts/Dinner/DinnerResponse.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DelightDinner.Contracts.Dinner;
 
-namespace DelightDinner.Contracts.Dinner
+public record DinnerResponse(
+    string Id,
+    string Name,
+    string Description,
+    DateTime StartDateTime,
+    DateTime EndDateTime,
+    DateTime? StartedDateTime,
+    DateTime? EndedDateTime,
+    DinnerStatus Status,
+    int MaxGuests,
+    bool IsPublic,
+    Price Price,
+    string MenuId,
+    string HostId,
+    Uri ImageUrl,
+    Location Location,
+    DateTime CreatedDateTime,
+    DateTime UpdatedDateTime);
+
+public enum DinnerStatus
 {
-    internal class DinnerResponse
-    {
-    }
+    Upcoming, 
+    InProgress,
+    Ended,
+    Canceled
 }
+
+public record Price(
+    decimal Amount,
+    string Currency);
+
+public record Location(
+    string Name,
+    string Address,
+    string Latitude,
+    string Longtitude);
