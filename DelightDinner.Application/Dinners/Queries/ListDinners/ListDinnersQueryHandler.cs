488def53094f2d3d1bf92596da35321aeeac8375
andriikorshenko <andriikorshenko@gmail.com>
2023-05-05 20:14:50 +0200

    Create a Handler for ListDinnersQuery

---
+++ DelightDinner.Application/Dinners/Queries/ListDinners/ListDinnersQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using DelightDinner.Application.Common.Interfaces.Persistence;
+using DelightDinner.Domain.Dinner;
+using DelightDinner.Domain.Host.ValueObjects;
+
+using ErrorOr;
+using MediatR;
+
+namespace DelightDinner.Application.Dinners.Queries.ListDinners;
+
+public class ListDinnerQueryHandler 
+    : IRequestHandler<ListDinnersQuery, ErrorOr<List<Dinner>>>
+{
+    private readonly IDinnerRepository _dinnerRepository;
+
+    public ListDinnerQueryHandler(IDinnerRepository dinnerRepository)
+    {
+        _dinnerRepository = dinnerRepository;
+    }
+
+    public async Task<ErrorOr<List<Dinner>>> Handle(
+        ListDinnersQuery query, 
+        CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
+
+        var hostId = HostId.Create(query.HostId);
+        return _dinnerRepository.List(hostId);
+    }
+}
