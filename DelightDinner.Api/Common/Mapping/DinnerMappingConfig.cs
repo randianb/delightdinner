7d7d08f51487a365037b8854d9f35090bf3e1a9c
andriikorshenko <andriikorshenko@gmail.com>
2023-05-05 20:50:52 +0200

    Create DinnerMappingConfig in Api layer

---
+++ DelightDinner.Api/Common/Mapping/DinnerMappingConfig.cs
@@ -0,0 +1,27 @@
+﻿using DelightDinner.Application.Dinners.Commands.CreateDinner;
+using DelightDinner.Application.Dinners.Queries.ListDinners;
+using DelightDinner.Contracts.Dinner;
+using DelightDinner.Domain.Dinner;
+
+using Mapster;
+
+namespace DelightDinner.Api.Common.Mapping;
+
+public class DinnerMappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<(CreateDinnerRequest Request, string HostId), CreateDinnerCommand>()
+            .Map(dest => dest.HostId, src => src.HostId)
+            .Map(dest => dest, src => src.Request);
+
+        config.NewConfig<string, ListDinnersQuery>()
+            .MapWith(src => new ListDinnersQuery(src));
+
+        config.NewConfig<Dinner, CreateDinnerResponse>()
+            .Map(dest => dest.Id, src => src.Id.Value.ToString())
+            .Map(dest => dest.Status, src => src.Status.Value)
+            .Map(dest => dest.MenuId, src => src.MenuId.Value)
+            .Map(dest => dest.HostId, src => src.HostId.Value);
+    }
+}
