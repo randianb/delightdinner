9f0ec2f75ea31b7c968965fa574bca59ea44b1e5
andriikorshenko <andriikorshenko@gmail.com>
2023-05-05 19:42:27 +0200

    Create Dinner Command

---
+++ DelightDinner.Application/Dinners/Commands/CreateDinner/CreateDinnerCommand.cs
@@ -0,0 +1,29 @@
+﻿using DelightDinner.Domain.Dinner;
+
+using ErrorOr;
+using MediatR;
+
+namespace DelightDinner.Application.Dinners.Commands.CreateDinner;
+
+public record CreateDinnerCommand(
+    string HostId,
+    string Name,
+    string Description,
+    DateTime StartDateTime,
+    DateTime EndDateTime,
+    bool IsPublic,
+    int MaxGuests,
+    DinnerPriceCommand Price,
+    string MenuId,
+    Uri ImageUri,
+    DinnerLocationCommand Location) : IRequest<ErrorOr<Dinner>>;
+
+public record DinnerPriceCommand(
+    decimal Amount,
+    string Currency);
+
+public record DinnerLocationCommand(
+    string Name,
+    string Address,
+    double Latitude,
+    double Longtitude);
