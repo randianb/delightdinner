2e873b81824ef69ba0af1dcc20a93d554c7c1b82
andriikorshenko <andriikorshenko@gmail.com>
2023-05-20 19:51:37 +0200

    Update Migration

--- DelightDinner.Domain/Common/Models/IHasDomainEvents.cs
+++ DelightDinner.Domain/Common/Models/IHasDomainEvents.cs
@@ -1,7 +1,10 @@
 ﻿namespace DelightDinner.Domain.Common.Models;
+
 public interface IHasDomainEvents
 {
     public IReadOnlyList<IDomainEvent> DomainEvents { get; }
+
     public void AddDomainEvent(IDomainEvent domainEvent);
+
     public void ClearDomainEvents();
 }
