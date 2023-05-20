1b6de9a9e92be487fde52069e532af8cfdd84df6
andriikorshenko <andriikorshenko@gmail.com>
2023-05-20 18:56:37 +0200

    Update migration

---
+++ DelightDinner.Infrastructure/Migrations/20230519170737_InitialUpdate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DelightDinner.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
