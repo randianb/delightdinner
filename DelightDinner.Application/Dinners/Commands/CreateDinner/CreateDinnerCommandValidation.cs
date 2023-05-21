139bb855f6635a7370efe2bf72bfe7d0c8426c03
andriikorshenko <andriikorshenko@gmail.com>
2023-05-21 10:23:48 +0200

    Add CreateDinnerCommandValidation

---
+++ DelightDinner.Application/Dinners/Commands/CreateDinner/CreateDinnerCommandValidation.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace DelightDinner.Application.Dinners.Commands.CreateDinner;
+
+public class CreateDinnerCommandValidation : AbstractValidator<CreateDinnerCommand>
+{
+    public CreateDinnerCommandValidation()
+    {
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.Description).NotEmpty();
+        RuleFor(x => x.IsPublic).NotEmpty();
+        RuleFor(x => x.MaxGuests).NotEmpty();
+        RuleFor(x => x.Price).NotNull();
+        RuleFor(x => x.MenuId).NotEmpty();
+        RuleFor(x => x.ImageUrl).NotEmpty();
+        RuleFor(x => x.Location).NotNull();
+    }
+}
