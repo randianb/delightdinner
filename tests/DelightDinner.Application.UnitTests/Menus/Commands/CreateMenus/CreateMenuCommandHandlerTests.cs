5b15582c6bfe4f4ce9f74341369ffc638cf04df6
andriikorshenko <andriikorshenko@gmail.com>
2023-05-23 21:55:56 +0200

    Create a UnitTest for CreateMenuHandler

---
+++ tests/DelightDinner.Application.UnitTests/Menus/Commands/CreateMenus/CreateMenuCommandHandlerTests.cs
@@ -0,0 +1,13 @@
+﻿namespace DelightDinner.Application.UnitTests.Menus.Commands.CreateMenus;
+
+public class CreateMenuCommandHandlerTests
+{
+    public void HandleCreateMenuCommand_WhenMenuIsValid_ShouldCreateMenu()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
---
+++ tests/DelightDinner.Application.UnitTests/Menus/Commands/TestUtils/CreateMenuCommandUtils.cs
@@ -0,0 +1,29 @@
+﻿using DelightDinner.Application.Menus.Commands.CreateMenu;
+using DelightDinner.Application.UnitTests.TestUtils.Constants;
+
+namespace DelightDinner.Application.UnitTests.Menus.Commands.TestUtils;
+
+public static class CreateMenuCommandUtils
+{
+    public static CreateMenuCommand CreateCommand() =>
+        new CreateMenuCommand(
+            Constants.Host.Id.ToString()!,
+            Constants.Menu.Name,
+            Constants.Menu.Description,
+            CreateSectionsCommand());
+
+    public static List<CreateMenuSectionCommand> CreateSectionsCommand(int sectionCount = 1) => 
+        Enumerable.Range(0, sectionCount)
+            .Select(index => new CreateMenuSectionCommand(
+                Constants.Menu.SectionNameFromIndex(index),
+                Constants.Menu.SectionDescriptionFromIndex(index),
+                CreateItemsCommand()))
+            .ToList();
+
+    public static List<CreateMenuItemCommand> CreateItemsCommand(int itemCount = 1) =>
+        Enumerable.Range(0, itemCount)
+            .Select(index => new CreateMenuItemCommand(
+                Constants.Menu.ItemNameFromIndex(index),
+                Constants.Menu.ItemDescriptionFromIndex(index)))
+            .ToList();
+}
