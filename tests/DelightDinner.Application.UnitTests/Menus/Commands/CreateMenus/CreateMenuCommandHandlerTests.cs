bf932a3e200d2bb5bc6ce835e33a012b0337adae
andriikorshenko <andriikorshenko@gmail.com>
2023-05-28 12:25:18 +0200

    Add validation for CreateMenuCommandHandlerTests

--- tests/DelightDinner.Application.UnitTests/Menus/Commands/CreateMenus/CreateMenuCommandHandlerTests.cs
+++ tests/DelightDinner.Application.UnitTests/Menus/Commands/CreateMenus/CreateMenuCommandHandlerTests.cs
@@ -1,13 +1,37 @@
-﻿namespace DelightDinner.Application.UnitTests.Menus.Commands.CreateMenus;
+﻿using DelightDinner.Application.Common.Interfaces.Persistence;
+using DelightDinner.Application.Menus.Commands.CreateMenu;
+using DelightDinner.Application.UnitTests.Menus.Commands.TestUtils;
+using DelightDinner.Application.UnitTests.TestUtils.Menus.Extensions;
+
+using FluentAssertions;
+using Moq;
+
+namespace DelightDinner.Application.UnitTests.Menus.Commands.CreateMenus;
 
 public class CreateMenuCommandHandlerTests
 {
-    public void HandleCreateMenuCommand_WhenMenuIsValid_ShouldCreateMenu()
+    private readonly CreateMenuCommandHandler _handler;
+    private readonly Mock<IMenuRepository> _mockMenuRepository;
+
+    public CreateMenuCommandHandlerTests()
+    {
+        _mockMenuRepository = new Mock<IMenuRepository>();
+        _handler = new CreateMenuCommandHandler(_mockMenuRepository.Object);
+    }
+
+    public async Task HandleCreateMenuCommand_WhenMenuIsValid_ShouldCreateMenu()
     {
         // Arrange
+        // Create the command
+        var createMenuCommand = CreateMenuCommandUtils.CreateCommand();
 
         // Act
+        // Invoke the handler
+        var result = await _handler.Handle(createMenuCommand, default);
 
         // Assert
+        result.IsError.Should().BeFalse();
+
+        result.Value.ValidateCreatedFrom(createMenuCommand);
     }
 }
---
+++ tests/DelightDinner.Application.UnitTests/TestUtils/Menus/Extensions/MenuExtensions.Validations.cs
@@ -0,0 +1,34 @@
+﻿using DelightDinner.Application.Menus.Commands.CreateMenu;
+using DelightDinner.Domain.MenuAggregate;
+using DelightDinner.Domain.MenuAggregate.Entities;
+
+using FluentAssertions;
+
+namespace DelightDinner.Application.UnitTests.TestUtils.Menus.Extensions;
+
+public static partial class MenuExtensions
+{
+    public static void ValidateCreatedFrom(this Menu menu, CreateMenuCommand command)
+    {
+        menu.Name.Should().Be(command.Name);
+        menu.Description.Should().Be(command.Description);
+        menu.Sections.Should().HaveSameCount(command.Sections);
+        menu.Sections.Zip(command.Sections).ToList().ForEach(pair => ValidateSection(pair.First, pair.Second));
+
+        static void ValidateSection(MenuSection section, CreateMenuSectionCommand command)
+        {
+            section.Id.Should().NotBeNull();
+            section.Name.Should().Be(command.Name);
+            section.Description.Should().Be(command.Description);
+            section.Items.Should().HaveSameCount(command.Items);
+            section.Items.Zip(command.Items).ToList().ForEach(pair => ValidateItem(pair.First, pair.Second));
+        }
+
+        static void ValidateItem(MenuItem item, CreateMenuItemCommand command)
+        {
+            item.Id.Should().NotBeNull();
+            item.Name.Should().Be(command.Name);
+            item.Description.Should().Be(command.Description);
+        }
+    }
+}
