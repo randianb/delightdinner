3264801ef3e1c817e3115f91ae4fd089edd7b8e0
andriikorshenko <andriikorshenko@gmail.com>
2023-06-18 10:53:03 +0200

    Add DinnerCreatedEventHandler for Hosts

--- src/DelightDinner.Application/Hosts/Events/DinnerCreatedEventHandler.cs
+++ src/DelightDinner.Application/Hosts/Events/DinnerCreatedEventHandler.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DelightDinner.Application.Common.Interfaces.Persistence;
+using DelightDinner.Domain.DinnerAggregate.Events;
+using DelightDinner.Domain.DinnerAggregate.ValueObjects;
+using DelightDinner.Domain.HostAggregate;
 
-namespace DelightDinner.Application.Hosts.Events
+using MediatR;
+
+namespace DelightDinner.Application.Hosts.Events;
+
+public class DinnerCreatedEventHandler : INotificationHandler<DinnerCreated>
 {
-    internal class DinnerCreatedEventHandler
+    private readonly IHostRepository _hostRepository;
+
+    public DinnerCreatedEventHandler(IHostRepository hostRepository)
     {
+        _hostRepository = hostRepository;
+    }
+
+    public async Task Handle(
+        DinnerCreated dinnerCreatedEvent, 
+        CancellationToken cancellationToken)
+    {
+        if (await _hostRepository.GetByIdAsync(dinnerCreatedEvent.Dinner.HostId) is not Host host)
+        {
+            throw new InvalidOperationException($"Dinner has invalid host id (dinner id: {dinnerCreatedEvent.Dinner.Id}, host id: {dinnerCreatedEvent.Dinner.HostId}).");
+        }
+
+        host.AddDinnerId((DinnerId)dinnerCreatedEvent.Dinner.Id);
+
+        await _hostRepository.UpdateAsync(host);
     }
 }
