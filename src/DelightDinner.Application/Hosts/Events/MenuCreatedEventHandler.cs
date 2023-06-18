3c0ae0cccbfbd2a01ff29f14af102afec748b09c
andriikorshenko <andriikorshenko@gmail.com>
2023-06-18 10:43:47 +0200

    Add MenuCreatedEventHandler for Hosts

--- src/DelightDinner.Application/Hosts/Events/MenuCreatedEventHandler.cs
+++ src/DelightDinner.Application/Hosts/Events/MenuCreatedEventHandler.cs
@@ -1,4 +1,7 @@
-﻿using DelightDinner.Domain.MenuAggregate.Events;
+﻿using DelightDinner.Application.Common.Interfaces.Persistence;
+using DelightDinner.Domain.HostAggregate;
+using DelightDinner.Domain.MenuAggregate.Events;
+using DelightDinner.Domain.MenuAggregate.MenuObjects;
 
 using MediatR;
 
@@ -13,8 +16,17 @@ public class MenuCreatedEventHandler : INotificationHandler<MenuCreated>
         _hostRepository = hostRepository;
     }
 
-    public Task Handle(MenuCreated notification, CancellationToken cancellationToken)
+    public async Task Handle(
+        MenuCreated menuCreatedEvent, 
+        CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        if (await _hostRepository.GetByIdAsync(menuCreatedEvent.Menu.HostId) is not Host host)
+        {
+            throw new InvalidOperationException($"Menu has invalid host id (menu id: {menuCreatedEvent.Menu.Id}, host id: {menuCreatedEvent.Menu.HostId}).");
+        }
+
+        host.AddMenuId((MenuId)menuCreatedEvent.Menu.Id);
+
+        await _hostRepository.UpdateAsync(host);
     }
 }
